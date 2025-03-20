787f3c29a3fcf773aefb653550d5df9f00d51601
Arman <contact.armankar@gmail.com>
2025-03-20 21:44:49 +0530

    sub-optimal code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuthForms from "./components/auth-form";
 import LandingPage from "./pages/landing-page";
-import CustomerDashboard from "./pages/customer/customer-dashboard";
-import OwnerDashboard from "./pages/owner/owner-dashboard";
 
-function App() {
+const App = () => {
     return (
         <Router>
             <Routes>
@@ -12,11 +10,6 @@ function App() {
                 <Route path="/auth/login" element={<AuthForms />} />
                 <Route path="/auth/signup" element={<AuthForms />} />
                 <Route path="/" element={<LandingPage />} />
-                <Route
-                    path="/customer-dashboard"
-                    element={<CustomerDashboard />}
-                />
-                <Route path="/owner-dashboard" element={<OwnerDashboard />} />
             </Routes>
         </Router>
     );
